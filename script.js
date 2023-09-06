d331f5ed29ea9cdc1d59acb3d7c65e8d406d4159
Rbsilva2023 <robsonchristopher2022@gmail.com>
2023-09-06 19:11:00 -0300

    part1 concluida

--- script.js
+++ script.js
@@ -1,6 +1,20 @@
-function isPalindrome(str){
-    /* Seu código aqui */
-}
+function isPalindrome(Radar) {
+
+    const fraseTratada = Radar.replaceAll(' ', '').toLowerCase();
+    const length = fraseTratada.length;
+
+    
+        for (let inicio = 0, fim = length -1; inicio < fim; inicio++, fim--) {
+        
+            if (fraseTratada[inicio] !== fraseTratada[fim]) {
+                return false;
+            }
+        }
+        return true;
+    }
+    
+
+console.log(isPalindrome("Radar"));
 
 function arrayMaxMin(arr){
     /* Seu código aqui */
